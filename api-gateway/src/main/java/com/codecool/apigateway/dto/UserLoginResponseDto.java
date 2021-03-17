3fd15d2ae69865de96106994a08938a030a9d33a
Marius Ceobanu <ceobanu.marius@gmail.com>
2021-03-17 16:07:16 +0200

    Implement Login system CF-20

---
+++ api-gateway/src/main/java/com/codecool/apigateway/dto/UserLoginResponseDto.java
@@ -0,0 +1,19 @@
+package com.codecool.apigateway.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginResponseDto {
+    private String username;
+    private List<String> roles = new ArrayList<>();
+    private String token;
+}
